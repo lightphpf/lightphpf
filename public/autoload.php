58ebce8a541030737a4ef4c8c778b00d28e4de3d
Soulaimane yahya <soulaimaneyh07@gmail.com>
2023-03-14 13:33:25 +0100

    fixing - conf twig file

--- public/autoload.php
+++ public/autoload.php
@@ -16,3 +16,4 @@ error_reporting(E_ALL);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 $config = require_once __DIR__ . '/../config/app.php';
+$twig = require_once __DIR__ . '/../config/twig.php';
--- public/index.php
+++ public/index.php
@@ -6,14 +6,7 @@
  */
 
 use App\Init;
-use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
 
 require_once __DIR__ . '/autoload.php';
-
-$twigLoader = new FilesystemLoader($config['app_root'] . '/Views');
-$twig = new Environment($twigLoader, [
-    'cache' => $config['app_root'] . '/Bootstrap/Cache',
-]);
 
 $init = new Init($config, $twig);
